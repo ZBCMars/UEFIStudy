608fcee35b7ae46fc7022a430d2150028215b135
Bocheng_Zhang <42092820+ZBCMars@users.noreply.github.com>
2022-07-13 14:13:03 -0700

    Update log.h

--- ToyBootPkg/Log.h
+++ ToyBootPkg/Log.h
@@ -1,7 +1,12 @@
 #include <Uefi.h>
 #include <Library/UefiLib.h>
 #include <Library/UefiBootServicesTableLib.h>
+#include <Protocol/SimpleFileSystem.h>
+#include <Guid/FileInfo.h>
 
 #include "Setup.h"
 
 EFI_STATUS LogInitial(EFI_HANDLE ImageHandle);
+EFI_STATUS LogTip(CHAR8 *Message);
+EFI_STATUS LogError(EFI_STATUS Code);
+EFI_STATUS Close();
