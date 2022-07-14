64295844b185596479124bc9dc196abd3f171c2c
Bocheng_Zhang <42092820+ZBCMars@users.noreply.github.com>
2022-07-14 14:28:54 -0700

    Update Video.h

--- ToyBootPkg/Video.h
+++ ToyBootPkg/Video.h
@@ -2,7 +2,19 @@
 
 #include <Protocol/GraphicsOutput.h>
 
+typedef struct{
+    EFI_PHYSICAL_ADDRESS FrameBufferBase;
+    UINTN                FrameBufferSize;
+    UINT32               HorizontalResolution;
+    UINT32               VerticalResolution;
+    UINT32               PixelPerScanLine;
+} VIDEO_CONFIG;
+
 EFI_STATUS GetGopHandle(
     IN EFI_HANDLE ImageHandle,
     OUT EFI_GRAPHICS_OUTPUT_PROTOCOL **Gop
+);
+
+EFI_STATUS SetVideoMode(
+    IN EFI_GRAPHICS_OUTPUT_PROTOCOL *Gop
 );
