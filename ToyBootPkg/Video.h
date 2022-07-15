b41260b7f837b353a0a0b94e51d0ce90ba12ae9f
Bocheng_Zhang <42092820+ZBCMars@users.noreply.github.com>
2022-07-15 14:06:38 -0700

    Add new functions and struct BMP_CONFIG

--- ToyBootPkg/Video.h
+++ ToyBootPkg/Video.h
@@ -10,6 +10,15 @@ typedef struct{
     UINT32               PixelPerScanLine;
 } VIDEO_CONFIG;
 
+typedef struct{
+    UINTN Size;
+    UINTN PageSize;
+    UINTN Width;
+    UINTN Height;
+    UINTN Offset;
+    UINT64 PixelStart;
+} BMP_CONFIG;
+
 EFI_STATUS GetGopHandle(
     IN EFI_HANDLE ImageHandle,
     OUT EFI_GRAPHICS_OUTPUT_PROTOCOL **Gop
@@ -17,4 +26,16 @@ EFI_STATUS GetGopHandle(
 
 EFI_STATUS SetVideoMode(
     IN EFI_GRAPHICS_OUTPUT_PROTOCOL *Gop
+);
+
+EFI_STATUS BmpTransform(
+    IN EFI_PHYSICAL_ADDRESS BmpBase,
+    OUT BMP_CONFIG *BmpConfig
+);
+
+EFI_STATUS DrawBmp(
+    IN EFI_GRAPHICS_OUTPUT_PROTOCOL *Gop,
+    IN BMP_CONFIG BmpConfig,
+    IN UINTN X,
+    IN UINTN Y
 );
