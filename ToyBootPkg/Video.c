df318693661187e215f4a262051ad6ed66e25274
Bocheng_Zhang <42092820+ZBCMars@users.noreply.github.com>
2022-07-15 14:05:45 -0700

    Add BmpTransform() and DrawBmp

--- ToyBootPkg/Video.c
+++ ToyBootPkg/Video.c
@@ -88,3 +88,88 @@ EFI_STATUS SetVideoMode(
 
     return Status;
 }
+
+EFI_STATUS BmpTransform(
+    IN EFI_PHYSICAL_ADDRESS BmpBase,
+    OUT BMP_CONFIG *BmpConfig
+){
+    EFI_STATUS Status = EFI_SUCCESS;
+
+    struct bmp_header *bheader = (struct bmp_header *)BmpBase;
+
+    Print(L"File Size = %08lx.\n", bheader->file_size);
+
+    BmpConfig->Size = GetValue(BmpBase, 0x02, 4);
+    BmpConfig->PageSize = (BmpConfig->Size >> 12) + 1;
+    BmpConfig->Offset = GetValue(BmpBase, 0x0A, 4);
+    BmpConfig->Width = GetValue(BmpBase, 0x12, 4);
+    BmpConfig->Height = GetValue(BmpBase, 0x16, 4);
+
+    EFI_PHYSICAL_ADDRESS PixelStart;
+    Status = gBS->AllocatePages(
+        AllocateAnyPages,
+        EfiLoaderData,
+        BmpConfig->PageSize,
+        &PixelStart
+    );
+
+    #ifdef DEBUG
+    if(EFI_ERROR(Status)){
+        Print(L"ERROR:Failed to AllocatePages for PixelStart.\n");
+        return Status;
+    }
+    else{
+        Print(L"SUCCESS:AllocatePages for PixelStart.\n");
+    }
+    #endif
+
+    EFI_GRAPHICS_OUTPUT_BLT_PIXEL *PixelFromFile = (EFI_GRAPHICS_OUTPUT_BLT_PIXEL *)(BmpBase
+                                                    + BmpConfig->Offset
+                                                    + BmpConfig->Height * BmpConfig->Width * 4);
+    
+    EFI_GRAPHICS_OUTPUT_BLT_PIXEL *PixelToBuffer = (EFI_GRAPHICS_OUTPUT_BLT_PIXEL *)PixelStart;
+
+    for(UINTN i = 0; i < BmpConfig->Height; i++){
+        PixelFromFile -= BmpConfig->Width;
+        for(UINTN j = 0; j < BmpConfig->Width; j++){
+            *PixelToBuffer = *PixelFromFile;
+            PixelToBuffer++;
+            PixelFromFile++;
+        }
+        PixelFromFile -= BmpConfig->Width;
+    }
+
+    BmpConfig->PixelStart = PixelStart;
+
+    return Status;
+}
+
+EFI_STATUS DrawBmp(
+    IN EFI_GRAPHICS_OUTPUT_PROTOCOL *Gop,
+    IN BMP_CONFIG BmpConfig,
+    IN UINTN X,
+    IN UINTN Y
+){
+    EFI_STATUS Status = EFI_SUCCESS;
+
+    Status = Gop->Blt(
+        Gop,
+        (EFI_GRAPHICS_OUTPUT_BLT_PIXEL *)BmpConfig.PixelStart,
+        EfiBltBufferToVideo,
+        0,0,
+        X,Y,
+        BmpConfig.Width,BmpConfig.Height,0
+    );
+
+    #ifdef DEBUG
+    if(EFI_ERROR(Status)){
+        Print(L"ERROR:Failed to Blt Logo.\n");
+        return Status;
+    }
+    else{
+        Print(L"SUCCESS:Blt Logo.\n");
+    }
+    #endif
+
+    return Status;
+}
