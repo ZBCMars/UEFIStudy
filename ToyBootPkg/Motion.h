59b863c0b8ca12b154f8346dca499d0a31c62e2e
Bocheng_Zhang <42092820+ZBCMars@users.noreply.github.com>
2022-07-16 18:02:58 -0700

    Update Motion.h

--- ToyBootPkg/Motion.h
+++ ToyBootPkg/Motion.h
@@ -1,6 +1,8 @@
 #include "File.h"
 #include "Video.h"
 
+#define GAP 1
+
 EFI_STATUS VideoInit(
     IN EFI_HANDLE ImageHandle,
     OUT VIDEO_CONFIG *VideoConfig
@@ -8,4 +10,8 @@ EFI_STATUS VideoInit(
 
 EFI_STATUS DrawLogo(
     IN EFI_HANDLE ImageHandle
+);
+
+EFI_STATUS DrawStep(
+    IN UINTN Step
 );
