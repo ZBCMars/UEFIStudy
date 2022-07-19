3cc49c4b485e93189161dd07619d356a47e555be
Bocheng_Zhang <42092820+ZBCMars@users.noreply.github.com>
2022-07-19 13:56:51 -0700

    Update main.c

--- Kernel/main.c
+++ Kernel/main.c
@@ -1,5 +1,21 @@
-int _start(){
-    int a = 3;
-    int b = 4;
-    return a+b;
+#include "Type.h"
+
+int DrawPixel(UINT32 *VideoHandle, UINT32 Color, int X, int Y){
+    UINT32 *Pixel = VideoHandle;
+    Pixel += Y * 1440 + X;
+    *Pixel = Color;
+    return 0;
+}
+
+UINT64 _start(BOOT_CONFIG *BootConfig){
+    UINT64 PassBack = 0;
+    UINT32 DarkBlue = 0xFF18679A;
+    UINT32 *VideoHandle = (UINT32 *)(BootConfig->FrameBufferBase);
+    for(int i = 0; i < 900; i++){
+        for(int j = 0; j < 1440; j++){
+            DrawPixel(VideoHandle, DarkBlue, j, i);
+        }
+    }
+    PassBack = BootConfig->FrameBufferBase;
+    return PassBack;
 }
