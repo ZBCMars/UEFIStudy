db3ae0818b2982b02483315c95c2e4444653661a
Bocheng_Zhang <42092820+ZBCMars@users.noreply.github.com>
2022-07-20 14:45:24 -0700

    Add files via upload

---
+++ Kernel/Shell.c
@@ -0,0 +1,55 @@
+#include "Shell.h"
+
+BASE Base;
+
+int ShellInitial(){
+    Base.Setup.Height = 860;
+    Base.Setup.Width = 1400;
+    Base.Setup.X = 20;
+    Base.Setup.Y = 20;
+    Base.Setup.Outlines = 1;
+
+    BLOCK BackGround;
+    BackGround.Color = 0xFF000000;
+    BackGround.Start.X = 20;
+    BackGround.Start.Y = 20;
+    BackGround.End.X = 1420;
+    BackGround.End.Y = 880;
+    DrawBlock(BackGround);
+
+    if(Base.Setup.Outlines){
+        BLOCK Outline;
+        Outline.Color = 0xFFFFFFFF;
+        Outline.Start.X = 20;
+        Outline.Start.Y = 20;
+        Outline.End.X = 1420;
+        Outline.End.Y = 22;
+        DrawBlock(Outline);
+
+        Outline.Start.X = 20;
+        Outline.Start.Y = 68;
+        Outline.End.X = 1418;
+        Outline.End.Y = 70;
+        DrawBlock(Outline);
+
+        Outline.Start.X = 20;
+        Outline.Start.Y = 878;
+        Outline.End.X = 1420;
+        Outline.End.Y = 880;
+        DrawBlock(Outline);
+
+        Outline.Start.X = 20;
+        Outline.Start.Y = 22;
+        Outline.End.X = 22;
+        Outline.End.Y = 878;
+        DrawBlock(Outline);
+
+        Outline.Start.X = 1418;
+        Outline.Start.Y = 20;
+        Outline.End.X = 1420;
+        Outline.End.Y = 878;
+        DrawBlock(Outline);
+    }
+
+    return 0;
+}
---
+++ Kernel/Shell.h
@@ -0,0 +1,19 @@
+#include "Video.h"
+
+#pragma pack(1)
+
+typedef struct{
+    UINT32 Width;
+    UINT32 Height;
+    UINT32 X;
+    UINT32 Y;
+    UINT32 Outlines;
+} SETUP;
+
+typedef struct{
+    SETUP Setup;
+} BASE;
+
+#pragma pack()
+
+int ShellInitial();
