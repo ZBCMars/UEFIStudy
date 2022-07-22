85d712fcf75cc3a646334aedb5e12d35f0b46042
Bocheng_Zhang <42092820+ZBCMars@users.noreply.github.com>
2022-07-22 16:29:53 -0700

    Update Video.c

--- Kernel/Video.c
+++ Kernel/Video.c
@@ -2,10 +2,14 @@
 
 VIDEO_CONFIG *VideoConfig;
 UINT32 *VideoStart;
+BMP_CONFIG *AsciiBmp;
+UINT32 *AsciiStart;
 
 int VideoInitial(BOOT_CONFIG *BootConfig){
     VideoConfig = &BootConfig->VideoConfig;
     VideoStart = (UINT32 *)BootConfig->VideoConfig.FrameBufferBase;
+    AsciiBmp = &BootConfig->AsciiBmp;
+    AsciiStart = (UINT32 *)BootConfig->AsciiBmp.PixelStart;
 
     BLOCK BackGround;
     BackGround.Start.X = 0;
@@ -38,4 +42,33 @@ int DrawBlock(BLOCK Block)
         }
     }
     return 0;
+}
+
+int DrawLetter(UINT8 Ascii, POINT Destination){
+    UINT8 Index = Ascii - 32;
+    
+    if(Ascii < 32 || Ascii > 126){
+        Index = 127 - 32;
+    }
+    UINT32 *From = AsciiStart + (Index % 32) * LetterWidth
+                   + (Index / 32) * LetterHeight * AsciiBmp->Width;
+    UINT32 *To = VideoStart + Destination.X + Destination.Y * VideoConfig->PixelPerScanLine;
+    //UINT32 Color;
+    UINT32 Output = *AsciiStart;
+
+    for(int i = 0; i < LetterHeight; i++)
+    {
+        for(int j = 0; j < LetterWidth; j++)
+        {
+            //Color = *From;
+            //Color = ((Color & 0x000000ff) << 24 ) | ((Color & 0x0000ff00) << 8) | ((Color & 0x00ff0000) >> 8) | ((Color & 0xff000000 ) >> 24);
+            *To = *From;
+            From++;
+            To++;
+        }
+        From = From + AsciiBmp->Width - LetterWidth;
+        To = To + VideoConfig->PixelPerScanLine - LetterWidth;
+    }
+    
+    return Output;
 }
