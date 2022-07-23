2cc347874b958d779fc13a0e1258b23454a0f7b1
Bocheng_Zhang <42092820+ZBCMars@users.noreply.github.com>
2022-07-23 15:44:48 -0700

    Add PrintStr(), PrintHex(), PrintDec()

--- Kernel/Shell.c
+++ Kernel/Shell.c
@@ -20,7 +20,7 @@ int ShellInitial(){
     POINT Dest;
     Dest.X = 30;
     Dest.Y = 78;
-    UINT32 Output = DrawLetter('T', Dest);
+    //UINT32 Output = DrawLetter('T', Dest);
 
     if(Base.Setup.Outlines){
         BLOCK Outline;
@@ -54,6 +54,123 @@ int ShellInitial(){
         Outline.End.X = 1420;
         Outline.End.Y = 878;
         DrawBlock(Outline);
+    }
+    
+    Base.Head.Title[0] = 'T';
+    Base.Head.Title[1] = '0';
+    Base.Head.Title[2] = 'y';
+    Base.Head.Title[3] = 'O';
+    Base.Head.Title[4] = 'S';
+
+    POINT TitleStart;
+    TitleStart.X = (Base.Setup.Width - 20 * 5) / 2 + 22;
+    TitleStart.Y = 27;
+
+    for(int i = 0; i < 5; i++){
+        DrawLetter(Base.Head.Title[i], TitleStart);
+        TitleStart.X += 20;
+    }
+
+    SetCursor(0);
+
+    return 0;
+}
+
+int SetCursor(UINT8 Count){
+    Base.Cursor.Col += Count;
+    if(Base.Cursor.Col >= 69){
+        Base.Cursor.Row += Base.Cursor.Col / 69;
+        Base.Cursor.Col %= 69;
+    }
+    Base.Cursor.Pos.X = Base.Cursor.Start.X + Base.Cursor.Col * 20;
+    Base.Cursor.Pos.Y = Base.Cursor.Start.Y + Base.Cursor.Row * 36;
+
+    BLOCK Cursor;
+    Cursor.Color = 0xFFFFFFFF;
+    Cursor.Start.X = Base.Cursor.Pos.X + 1;
+    Cursor.Start.Y = Base.Cursor.Pos.Y + 30;
+    Cursor.End.X = Cursor.Start.X + 18;
+    Cursor.End.Y = Cursor.Start.Y + 3;
+    DrawBlock(Cursor);
+
+    return 0;
+}
+
+int PrintStr(char *String){
+    char *ToPrint = String;
+    int Len = 0;
+
+    while(*ToPrint != '\0'){
+        Len++;
+        ToPrint++;
+    }
+
+    for(int i = 0; i < Len; i++){
+        if(String[i] == '\n'){
+            DrawLetter(' ', Base.Cursor.Pos);
+            SetCursor(69-Base.Cursor.Col);
+            continue;
+        }
+        DrawLetter(String[i], Base.Cursor.Pos);
+        SetCursor(1);
+    }
+
+    return 0;
+}
+
+int PrintHex(UINT64 Number){
+    UINT64 Trans = Number;
+
+    DrawLetter('0', Base.Cursor.Pos);
+    SetCursor(1);
+    DrawLetter('x', Base.Cursor.Pos);
+    SetCursor(1);
+
+    char Hex[16];
+    int Len = 0;
+
+    for(int i = 0; i < 16; i++){
+        Hex[i] = Trans & 0xF;
+        if(Hex[i] >= 0 && Hex[i] <= 9){
+            Hex[i] += 48;
+        }
+        if(Hex[i] > 9 && Hex[i] < 16){
+            Hex[i] += 55;
+        }
+
+        Trans = Trans >> 4;
+        Len++;
+        if(Trans == 0){
+            break;
+        }
+    }
+
+    for(int i = 0; i < Len; i++){
+        DrawLetter(Hex[Len-i-1], Base.Cursor.Pos);
+        SetCursor(1);
+    }
+
+    return 0;
+}
+
+int PrintDec(UINT64 Number){
+    UINT64 Trans = Number;
+
+    int Dec[20];
+    int Len = 0;
+
+    for(int i = 0; i < 20; i++){
+        Dec[i] = (Trans % 10) + 48;
+        Trans = Trans / 10;
+        Len++;
+        if(Trans == 0){
+            break;
+        }
+    }
+
+    for(int i = 0; i < Len; i++){
+        DrawLetter(Dec[Len-i-1], Base.Cursor.Pos);
+        SetCursor(1);
     }
 
     return 0;
