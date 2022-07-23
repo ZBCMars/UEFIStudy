13cd241fba11bc178f33514804505ad6fc766ca6
Bocheng_Zhang <42092820+ZBCMars@users.noreply.github.com>
2022-07-23 15:43:32 -0700

    Update Kernel.c

--- Kernel/Kernel.c
+++ Kernel/Kernel.c
@@ -4,7 +4,16 @@ UINT64 KernelStart(BOOT_CONFIG *BootConfig){
     UINT64 PassBack = 0;
     VideoInitial(BootConfig);
     ShellInitial();
+    
+    char String[20];
+    String[0] = 'z';
+    String[1] = 'b';
+    String[2] = 'c';
+    String[3] = '\n';
+    String[4] = '\0';
+    PrintStr(String);
 
+    PrintHex(0x123456789ABCDEF);
 
     return PassBack;
 }
